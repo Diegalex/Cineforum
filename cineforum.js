07f096f8ff93855788a2d8669da4d5853cb57ee2
Diegalex <diegoalejandrovera2003@gmail.com>
2026-01-12 10:42:22 +0100

    la primera funcion de js ya funciona

--- cineforum.js
+++ cineforum.js
@@ -1,7 +1,12 @@
 
-//no funciona todavia
 entradaSelect=document.getElementById("tipoSelec")
 entrada=document.getElementById("tipoEntrada")
 entrada.addEventListener("change",function(){
     entradaSelect.value=entrada.value
+})
+
+cantidadSelect=document.getElementById("cantidadSelec")
+cantidad=document.getElementById("cant")
+cantidad.addEventListener("change",function(){
+    cantidadSelect.value=cantidad.value
 })
