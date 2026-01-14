a92b52c042cd7b733a287159546b66d71609d8b1
Daria-18 <dariapetrov1807@gmail.com>
2026-01-14 14:17:04 +0100

    clase 14-1

--- cineforum.js
+++ cineforum.js
@@ -1,12 +1,28 @@
 
-entradaSelect=document.getElementById("tipoSelec")
-entrada=document.getElementById("tipoEntrada")
-entrada.addEventListener("change",function(){
-    entradaSelect.value=entrada.value
+entradaSelect = document.getElementById("tipoSelec")
+entrada = document.getElementById("tipoEntrada")
+entrada.addEventListener("change", function () {
+    entradaSelect.value = entrada.value
 })
 
-cantidadSelect=document.getElementById("cantidadSelec")
-cantidad=document.getElementById("cant")
-cantidad.addEventListener("change",function(){
-    cantidadSelect.value=cantidad.value
+cantidadSelect = document.getElementById("cantidadSelec")
+cantidad = document.getElementById("cant")
+cantidad.addEventListener("change", function () {
+    cantidadSelect.value = cantidad.value
 })
+
+formulario = document.getElementById("formCine");
+formulario.addEventListener("submit", function (e) {
+    //TODO
+    //Comprobar que todos los campos son correctos, else:
+    e.preventDefault();
+    alert("Introduce todas las opciones.")
+})
+
+// Para botonera -> Array con elementos de clase perteneciente a una clase de botones (quitar ids unicos)
+// Añadir estado (presionado/nopresionado), al presionar liberar todos menos self
+// (Por tanto solo permitir una misma sesióm)
+
+// DOM/DOM -> Resumen Entradas
+
+// Añadir metodos no vistos en clase
