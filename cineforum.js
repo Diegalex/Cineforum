1bb33f285be924af43eb0c65788de25d6cf00d8a
Diegalex <diegoalejandrovera2003@gmail.com>
2026-01-15 13:36:53 +0100

    añadidas algunas comprobaciones de js

--- cineforum.js
+++ cineforum.js
@@ -15,11 +15,17 @@ cantidad.addEventListener("change", function () {
     cantidadSelect.value = cantidad.value
 })
 */
+const formulario = document.getElementById("formCine");
+const nombre = document.getElementById("nombre");
+const correo = document.getElementById("email");
+const tipoEntrada = document.getElementById("tipoEntrada");
+const cantidad = document.getElementById("cant");
+
+let sesionSeleccionada = null; //para luego form
 
 //Tipo de entrada para el form
 function setTipoEntradas() {
     let entradaSelect = document.getElementById("tipoSelec");
-    let entrada = document.getElementById("tipoEntrada");
     entradaSelect.value = entrada.value;
     return entrada.value !== ""; // devuelve true si hay valor para luego comprobar en el submit
 }
@@ -28,23 +34,71 @@ document.getElementById("tipoEntrada").addEventListener("change", setTipoEntrada
 // Función que setea la cantidad de entradas en el form
 function setCantidadEntradas() {
     var cantidadSelect = document.getElementById("cantidadSelec");
-    var cantidad = document.getElementById("cant");
     cantidadSelect.value = cantidad.value;
     return cantidad.value !== "" && cantidad.value > 0; 
 }
 
 document.getElementById("cant").addEventListener("change", setCantidadEntradas);
 
-formulario = document.getElementById("formCine");
+//comprueba segun se va escribiendo el nombre e email si son validos te lo pone en verde, en caso contrario pone en rojo
+nombre.addEventListener("input", function(){
+    const nombreValor= nombre.value
+    if(nombreValor.length<4||!nombreValor.includes(" ")){
+        nombre.classList.add("msg-error");
+        nombre.classList.add("campo-error")
+        nombre.classList.remove("msg-ok")
+        nombre.classList.remove("campo-ok")
+    } 
+    else{
+        nombre.classList.add("msg-ok");
+        nombre.classList.add("campo-ok");
+        nombre.classList.remove("msg-error")
+        nombre.classList.remove("campo-error")
+    } 
+})
+ 
+//faltaria añadir alguna condicion mas en el coreo que no se me ocurre
+correo.addEventListener("input", function(){
+    const correoValor= correo.value
+    if(!correoValor.includes("@")){
+        correo.classList.add("msg-error");
+        correo.classList.add("campo-error");
+        correo.classList.remove("msg-ok")
+        correo.classList.remove("campo-ok")
+    } 
+    else{
+        correo.classList.add("msg-ok");
+        correo.classList.add("campo-ok");
+        correo.classList.remove("msg-error")
+        correo.classList.remove("campo-error")
+    } 
+})
+
+
 formulario.addEventListener("submit", function (e) {
     //TODO
     //Comprobar que todos los campos son correctos, else:
-    e.preventDefault();
-    alert("Introduce todas las opciones.")
+
+    mensaje=""
+    nombreValor=nombre.value
+    correoValor=correo.value
+    tipoValor = tipoEntrada.value;
+    cantidadValor = cantidad.value;
+    if(nombreValor.length<4||!nombreValor.includes(" ")) mensaje+="Nombre no valido\n"
+    if(!correoValor.includes("@")) mensaje+="correo no valido\n"
+    if(sesionSeleccionada==null) mensaje+="sellecione pelicula y sesion\n"
+    if (tipoValor === "") mensaje += "Seleccione tipo de entrada.\n";
+    if (cantidadValor === "" || cantidadValor <= 0) mensaje += "Seleccione cuántas entradas desea comprar.\n";
+    
+    //añadir else de que si esta todo bien te cree la entrada
+    if(mensaje.trim()!== ""){
+        alert(mensaje)
+        e.preventDefault();
+    }
+    
 })
 
 //Tarjeta de sesion funcionalidad de botón.
-sesionSeleccionada = null; //para luego form
 
 function seleccionarSesion(card) {
 
